d3a96db04d05284db322781cf5876dedb216074c
Gopal Dixit <gopaldixit9450@gmail.com>
2023-08-04 21:20:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import Restaurent from "./Component/Basics/Restaurent";
+// const App = () => {
+//   return (
+//     // <Restaurent/>
+//     <p>Hello</p>
+//   )
+// }
+
+// export default App
